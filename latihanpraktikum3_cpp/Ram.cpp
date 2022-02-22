ae1bc276e1f826a73b6e1ed083ac6ff402ddc351
Alief Muhammad Abdillah <91062027+aliefabdillah@users.noreply.github.com>
2022-02-22 20:00:44 +0700

    Add files via upload

---
+++ latihanpraktikum3_cpp/Ram.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Ram
+{
+private:
+    /* data */
+    string ramCapacity;
+    long ramPrice;
+public:
+// contructor
+    Ram() {}
+    Ram(string capacity, long price) {
+        this->ramCapacity = capacity;
+        this->ramPrice = price;
+    }
+
+// set and get atribut
+    void setRamCapacity(string c){ this->ramCapacity = c; }
+    void setRamPrice(long p){ this->ramPrice = p; }
+
+    string getRamCapacity(){ return this->ramCapacity; }
+    long getRamPrice(){ return this->ramPrice; }
+
+    /* method print ram */
+    void printRam(){
+        cout << "-Data RAM" << endl;
+        cout << " RAM Capicity : " << this->ramCapacity << endl;
+        cout << " RAM Price    : " << this->ramPrice << endl;
+        cout << "-----------------------------------" << endl;
+    }
+
+    ~Ram() {}
+};
